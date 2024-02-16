395e729239ae4ac7fdd5755e9faadf66c27ec32f
Гамзат <abdullayevgamzat@gmail.com>
2024-02-17 00:58:23 +0400

    spring_security: start

--- src/main/java/com/bank/greenway/repository/CustomerRepository.java
+++ src/main/java/com/bank/greenway/repository/CustomerRepository.java
@@ -6,5 +6,5 @@ import com.bank.greenway.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    List<Customer> findBylastName(String lastname);
+    List<Customer> findBylastname(String lastname);
 }
